a32b3688eb282a97485d0000daa69d353a9a948d
turgunov24 <learningplatforms.turgunov@gmail.com>
2022-07-17 16:20:35 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="App w-full h-screen ">
     </div>
   );
 }
