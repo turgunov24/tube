abcce15e3a9dfa931e3de23e1a1b36cf3f04ac01
turgunov24 <learningplatforms.turgunov@gmail.com>
2022-07-18 18:20:47 +0500

    asdasd

--- src/components/pages/home-page/home-page.jsx
+++ src/components/pages/home-page/home-page.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
-import NavbarTop from '../../navbar/navbar-top/navbar-top';
+import React from "react";
+import "./home-page.css"
+import NavbarTop from "../../navbar/navbar-top/navbar-top";
+
+//additional
+import { keywords } from "../../../assets/links/keyword"
+
+
 
 
 function HomeSection() {
-    return ( 
-        <section className='w-full h-screen flex flex-col'>
-            <NavbarTop />
-            <div id="home-page-main" className='border w-full flex-grow flex'>
-
-            </div>
-        </section>
-     );
+  return (
+    <section className="w-full h-screen flex flex-col">
+      <NavbarTop />
+      <div id="home-page-main" className="border flex-grow flex">
+        <div id="home-page-keywords" className="border h-10 flex-grow flex items-center gap-5">
+            {keywords.map((keyword,key)=>(
+                <div key={key}>
+                    {keyword.name}
+                </div>
+            ))}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default HomeSection;
